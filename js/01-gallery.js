f226bd82fc253165767be93e05d1790547363f14
Max-Arslanov <133591144+Max-Arslanov@users.noreply.github.com>
2023-11-13 15:57:20 +0200

    markup created

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const list = document.querySelector(".gallery");
+const markup = galleryItems.map ((galleryItem) => `<li class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="small-image.jpg"
+    data-source="large-image.jpg"
+    alt="Image description"
+  />
+</a>
+</li>`)
+.join("");
+list.innerHTML = markup;
 console.log(galleryItems);
