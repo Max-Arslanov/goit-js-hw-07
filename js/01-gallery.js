deb6da66e483e442b01b04c121b03075cfced834
Max-Arslanov <133591144+Max-Arslanov@users.noreply.github.com>
2023-11-13 16:08:23 +0200

    Links corrected

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,12 +2,12 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const list = document.querySelector(".gallery");
 const markup = galleryItems.map ((galleryItem) => `<li class="gallery__item">
-<a class="gallery__link" href="large-image.jpg">
+<a class="gallery__link" href="${galleryItem.original}">
   <img
     class="gallery__image"
-    src="small-image.jpg"
-    data-source="large-image.jpg"
-    alt="Image description"
+    src="${galleryItem.preview}"
+    data-source="${galleryItem.original}"
+    alt="${galleryItem.description}"
   />
 </a>
 </li>`)
