fccd19172b81a9f4b552c7dd9584079740d484b6
Max-Arslanov <133591144+Max-Arslanov@users.noreply.github.com>
2023-11-14 19:53:30 +0200

    Lightbox Gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems.map ((galleryItem) => `<li class="gallery__item">
+<a class="gallery__link" href="${galleryItem.original}">
+   <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+</a>
+</li>`)
+.join("");
+list.innerHTML = markup;
+document.addEventListener('DOMContentLoaded', function () {
+    const gallery = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionDelay: 250,
+    });
+  });
